32c0defb7bc38247f473a59a6baa035c53336c13
zhitinglu@outlook.com <luzhiting@outlook.com>
2020-10-23 13:34:35 +0200

    added post put delete request and express handlebars

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const path = require('path');
 const bodyParser = require('body-parser');
+const exphbs = require('express-handlebars');
+const members = require('./Members');
 
 // const logger = require('./middleware/logger');
 
@@ -12,23 +14,36 @@ const app = express();
 //Init middeleware
 // app.use(logger);
 
+// Handlebars middleware
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
 //Body Parser Middleware
 app.use(bodyParser.json());
 
+//app.use(express.json());
+//app.use(express.urlencoded({ extended: false}));
+
+app.get('/', (req, res) => res.render('index', {
+    title: 'Member app',
+    members
+}));
 
 //set static folder
 app.use(express.static(path.join(__dirname, 'public')));
 // app.use(express.static('public')); 
-// Are the above the same? 
+// Are the above the same?
 
 /*
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 */
+
+
 const router = require('./routes/api/members');
 app.use('/api/members', router);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => console.log(`App is listening on port ${PORT}`));
--- routes/api/members.js
+++ routes/api/members.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const uuid = require('uuid');
 const router = express.Router();
 const members = require('../../Members');
 
@@ -18,9 +19,60 @@ router.get('/:id', (req, res) => {
     
 });
 
-// Creat Member
+// Creates Member
 router.post('/', (req, res) => {
-    res.send(req.body);
+    const newMember = {
+        id: uuid.v4(),
+        name: req.body.name,
+        email: req.body.email,
+        status: 'active'
+    }
+
+    if (!newMember.name || !newMember.email) {
+      return  res.status(400).send('Please include a name and email');
+    } 
+    // mongodb: members.save(newMember)
+    members.push(newMember);
+    res.json(members);
+    // res.redirect('/');
 });
+
+// Updates member
+router.put('/:id', (req, res) => {
+    const found = members.some(member => member.id === parseInt(req.params.id));
+
+    if (found) {
+        const updateMember = req.body;
+        members.forEach(member => {
+            if (member.id === parseInt(req.params.id)) {
+                member.name = updateMember.name ? updateMember.name : member.name;
+                member.email = updateMember.email ? updateMember.email : member.email;   
+                
+                res.json( {
+                    message: "Member was updated", members
+                });
+            }
+        });
+    } else {
+        res.status(400).json({message: `No member with the id of ${req.params.id}`});
+    }
+    
+});
+
+// Deletes member
+router.delete('/:id', (req, res) => {
+    const found = members.some(member => member.id === parseInt(req.params.id));
+
+    if (found) {
+        res.json({
+            message: "Member deleted",
+            members: members.filter(member => member.id !== parseInt(req.params.id))
+        });
+    } else {
+        res.status(400).json({message: `No member with the id of ${req.params.id}`});
+    }
+    
+});
+
 
 module.exports = router;
